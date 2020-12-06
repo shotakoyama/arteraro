cba9f0d6c9464c3e74016978cdd5400590f5de1d
nymwa <nymwa0@gmail.com>
2020-12-06 17:55:53 +0900

    fix hyphen error generating rules

--- erarigilo/erarigilo/en/writing_system/punct/hyphen.py
+++ erarigilo/erarigilo/en/writing_system/punct/hyphen.py
@@ -1,14 +1,11 @@
 from erarigilo.util import *
 from erarigilo.en.util.token import EnToken
 
-en_dash = chr(0x2013)
-em_dash = chr(0x2014)
-
 class HyphenChoiceSamplingTokenWiseMistaker(ChoiceSamplingTokenWiseMistaker):
     def __init__(self):
         super().__init__('hyphen',
-                ['', '--', en_dash, em_dash],
-                p = [0.85, 0.1, 0.025, 0.025])
+                ['', '--', '---', '- -'],
+                p = [0.85, 0.10, 0.025, 0.025])
 
     def cond(self, token):
         return token.cor == '-'
@@ -17,37 +14,17 @@ class HyphenChoiceSamplingTokenWiseMistaker(ChoiceSamplingTokenWiseMistaker):
 class TwoHyphensChoiceSamplingTokenWiseMistaker(ChoiceSamplingTokenWiseMistaker):
     def __init__(self):
         super().__init__('two_hyphens',
-                ['', '-', en_dash, em_dash],
+                ['', '-', '---', '- -'],
                 p = [0.2, 0.7, 0.05, 0.05])
 
     def cond(self, token):
         return token.cor == '--'
 
 
-class EnDashChoiceSamplingTokenWiseMistaker(ChoiceSamplingTokenWiseMistaker):
-    def __init__(self):
-        super().__init__('en_dash',
-                ['', '-', '--', em_dash],
-                p = [0.2, 0.3, 0.3, 0.2])
-
-    def cond(self, token):
-        return token.cor == en_dash
-
-
-class EmDashChoiceSamplingTokenWiseMistaker(ChoiceSamplingTokenWiseMistaker):
-    def __init__(self):
-        super().__init__('em_dash',
-                ['', '-', '--', en_dash],
-                p = [0.2, 0.3, 0.3, 0.2])
-
-    def cond(self, token):
-        return token.cor == em_dash
-
-
 class HyphenInsertingIndexWiseMistaker(ChoiceSamplingIndexWiseMistaker):
     def __init__(self):
         super().__init__('ins_hyphen',
-                ['-', '--', en_dash, em_dash],
+                ['-', '--', '---', '- -'],
                 p = [0.7, 0.25, 0.025, 0.025])
 
     def c1(self, sent, index):
@@ -97,16 +74,6 @@ def _hyphen(dct):
 @register('two_hyphens')
 def _two_hyphens(dct):
     generator = TokenWiseGenerator(TwoHyphensChoiceSamplingTokenWiseMistaker)
-    return generator(dct)
-
-@register('en_dash')
-def _en_dash(dct):
-    generator = TokenWiseGenerator(EnDashChoiceSamplingTokenWiseMistaker)
-    return generator(dct)
-
-@register('em_dash')
-def _em_dash(dct):
-    generator = TokenWiseGenerator(EmDashChoiceSamplingTokenWiseMistaker)
     return generator(dct)
 
 @register('ins_hyphen')
