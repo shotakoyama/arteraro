ad618e46e619bd036247af0748b5cce69586d208
nymwa <nymwa0@gmail.com>
2021-02-09 20:03:41 +0900

    fix prod

--- fa_script/fa_script/util/prod.py
+++ fa_script/fa_script/util/prod.py
@@ -4,10 +4,11 @@ def make_index_list(config):
     return range(len(config['seed_list']))
 
 def make_epoch_list(config):
-    start = config['generate']['epoch'].get('start', 1)
-    step = config['generate']['epoch'].get('step', 1)
     max_epoch = config['train']['max_epoch']
-    epochs = range(start, max_epoch + 1, step)
+    start = config['generate']['epoch'].get('start', 1)
+    end = config['generate']['epoch'].get('end', max_epoch)
+    step = config['generate']['epoch'].get('step', 1)
+    epochs = range(start, end + 1, step)
     return epochs
 
 def make_prod(config):
